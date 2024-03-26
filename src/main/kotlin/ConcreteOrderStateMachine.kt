9fad72d8514ee69a07dce06e9d22dc25958aef82
aryumka <yooooar@gmail.com>
2024-03-26 23:52:27 +0900

    add concrete order state machine test

--- src/main/kotlin/ConcreteOrderStateMachine.kt
+++ src/main/kotlin/ConcreteOrderStateMachine.kt
@@ -17,39 +17,3 @@ class ConcreteOrderStateMachine (
     return transition[state]?.find { it.containsKey(event) }?.get(event)?.first ?: state
   }
 }
-
-//READY --> PLACED: PLACE_ORDER
-//PLACED --> PAID: PAY
-//PAID --> SHIPPED: SHIP
-//PAID --> CANCELLED: CANCEL
-//SHIPPED --> DELIVERED: DELIVER
-//SHIPPED --> PENDING: CANCEL
-//DELIVERED --> PENDING: CANCEL
-//PENDING --> CANCELLED: RETURN
-fun main() {
-  val transition = mapOf(
-    States.READY to listOf(
-      mapOf(Events.PLACE_ORDER to Pair(States.PLACED, Actions.OrderPlaced()))
-    ),
-    States.PLACED to listOf(
-      mapOf(Events.PAY to Pair(States.PAID, Actions.PaymentMade()))
-    ),
-    States.PAID to listOf(
-      mapOf(Events.SHIP to Pair(States.SHIPPED, Actions.OrderShipped())),
-      mapOf(Events.CANCEL to Pair(States.CANCELLED, Actions.orderCanceled()))
-    ),
-    States.SHIPPED to listOf(
-      mapOf(Events.DELIVER to Pair(States.DELIVERED, Actions.PaymentMade())),
-      mapOf(Events.CANCEL to Pair(States.PENDING, Actions.watingForReturn()))
-    ),
-    States.DELIVERED to listOf(
-      mapOf(Events.CANCEL to Pair(States.PENDING, Actions.watingForReturn()))
-    ),
-    States.PENDING to listOf(
-      mapOf(Events.RETURN to Pair(States.CANCELLED, Actions.orderCanceled()))
-    )
-  )
-
-  val orderStateMachine = ConcreteOrderStateMachine(transition)
-  orderStateMachine.transit(Events.PLACE_ORDER)
-}
--- src/test/kotlin/ConcreteOrderStateMachineTest.kt
+++ src/test/kotlin/ConcreteOrderStateMachineTest.kt
@@ -1,3 +1,33 @@
 import org.junit.jupiter.api.Assertions.*
+import type.Actions
+import type.Events
+import type.States
 
-class ConcreteOrderStateMachineTest
+class ConcreteOrderStateMachineTest {
+  val transition = mapOf(
+    States.READY to listOf(
+      mapOf(Events.PLACE_ORDER to Pair(States.PLACED, Actions.OrderPlaced()))
+    ),
+    States.PLACED to listOf(
+      mapOf(Events.PAY to Pair(States.PAID, Actions.PaymentMade()))
+    ),
+    States.PAID to listOf(
+      mapOf(Events.SHIP to Pair(States.SHIPPED, Actions.OrderShipped())),
+      mapOf(Events.CANCEL to Pair(States.CANCELLED, Actions.orderCanceled()))
+    ),
+    States.SHIPPED to listOf(
+      mapOf(Events.DELIVER to Pair(States.DELIVERED, Actions.PaymentMade())),
+      mapOf(Events.CANCEL to Pair(States.PENDING, Actions.watingForReturn()))
+    ),
+    States.DELIVERED to listOf(
+      mapOf(Events.CANCEL to Pair(States.PENDING, Actions.watingForReturn()))
+    ),
+    States.PENDING to listOf(
+      mapOf(Events.RETURN to Pair(States.CANCELLED, Actions.orderCanceled()))
+    )
+  )
+
+  val orderStateMachine = ConcreteOrderStateMachine(transition)
+//  orderStateMachine.transit(Events.PLACE_ORDER)
+
+}
