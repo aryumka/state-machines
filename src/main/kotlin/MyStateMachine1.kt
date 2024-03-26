9f4f4128aa673ec30a28918bddf94db200f3fb4e
aryumka <yooooar@gmail.com>
2024-03-26 23:28:59 +0900

    complete basic concrete order statemachine

--- src/main/kotlin/MyStateMachine1.kt
+++ src/main/kotlin/MyStateMachine1.kt
@@ -1,36 +1,69 @@
-// current state -> event, next state -> action
-// SOURCE 상태 - 이벤트 - TARGET 상태 - 액션
-class MyStateMachine1 {
-  class StateMachine (
-    private val transition: Map<States, List<Map<Events, Pair<States, Actions>>>>,
-  ) {
-    private var state: States = States.READY
+typealias TransitionTo = List<Map<Events, Pair<States, Actions>>>
 
-    fun setState(state: States) {
-      this.state = state
-    }
+class OrderStateMachine (
+  private val transition: Map<States, TransitionTo>,
+  private var state: States = States.READY
+) {
+  fun transit(event: Events): States {
+    val state = transition[state]?.find { it.containsKey(event) }?.get(event)?.first ?: States.INVALID
+    val action =  transition[state]?.find { it.containsKey(event) }?.get(event)?.second?.act()
+    println("Event: $event, State: $state, Action: $action")
+    this.state = state
 
-    fun transit(event: Events): States {
-      val action =  transition[state]?.find { it.containsKey(event) }?.get(event)?.second?.message ?: Actions.IGNORE.message
-      println("Event: $event, State: $state, Action: $action")
-
-      return transition[state]?.find { it.containsKey(event) }?.get(event)?.first ?: state
-    }
-  }
-
-  enum class States {
-    READY, PLACED, PAID, SHIPPED, DELIVERED, CANCELLED, PENDING, RETURNED
-  }
-
-  enum class Events {
-    PLACE_ORDER, PAY, SHIP, DELIVER, CANCEL, RETURN
-  }
-
-
-  enum class Actions(val message: String) {
-    ALLOW("allowed"), DENY("denied"), REFUND("refunded"), IGNORE("ignored");
+    return transition[state]?.find { it.containsKey(event) }?.get(event)?.first ?: state
   }
 }
 
+enum class States {
+  READY, PLACED, PAID, SHIPPED, DELIVERED, CANCELLED, PENDING, RETURNED, INVALID
+}
+
+enum class Events {
+  PLACE_ORDER, PAY, SHIP, DELIVER, CANCEL, RETURN
+}
+
+sealed class Actions {
+  abstract val message: String
+  fun act() { println(message) }
+  data class OrderPlaced(override val message: String = "order is placed"): Actions()
+  data class PaymentMade(override val message: String = "order is delivered"): Actions()
+  data class OrderShipped(override val message: String = "order is shipped"): Actions()
+  data class watingForReturn(override val message: String = "waiting for return"): Actions()
+  data class orderCanceled(override val message: String = "order is canceled"): Actions()
+}
+
+//READY --> PLACED: PLACE_ORDER
+//PLACED --> PAID: PAY
+//PAID --> SHIPPED: SHIP
+//PAID --> CANCELLED: CANCEL
+//SHIPPED --> DELIVERED: DELIVER
+//SHIPPED --> PENDING: CANCEL
+//DELIVERED --> PENDING: CANCEL
+//PENDING --> CANCELLED: RETURN
 fun main() {
+  val transition = mapOf(
+    States.READY to listOf(
+      mapOf(Events.PLACE_ORDER to Pair(States.PLACED, Actions.OrderPlaced()))
+    ),
+    States.PLACED to listOf(
+      mapOf(Events.PAY to Pair(States.PAID, Actions.PaymentMade()))
+    ),
+    States.PAID to listOf(
+      mapOf(Events.SHIP to Pair(States.SHIPPED, Actions.OrderShipped())),
+      mapOf(Events.CANCEL to Pair(States.CANCELLED, Actions.orderCanceled()))
+    ),
+    States.SHIPPED to listOf(
+      mapOf(Events.DELIVER to Pair(States.DELIVERED, Actions.PaymentMade())),
+      mapOf(Events.CANCEL to Pair(States.PENDING, Actions.watingForReturn()))
+    ),
+    States.DELIVERED to listOf(
+      mapOf(Events.CANCEL to Pair(States.PENDING, Actions.watingForReturn()))
+    ),
+    States.PENDING to listOf(
+      mapOf(Events.RETURN to Pair(States.CANCELLED, Actions.orderCanceled()))
+    )
+  )
+
+  val orderStateMachine = OrderStateMachine(transition)
+  orderStateMachine.transit(Events.PLACE_ORDER)
 }
