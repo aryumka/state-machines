01caf35d8dfeaf00f16bd7aaecfcb30be9a8bd8f
aryumka <yooooar@gmail.com>
2024-03-27 15:59:31 +0900

    fix the state to find the correct target

--- src/main/kotlin/ConcreteOrderStateMachine.kt
+++ src/main/kotlin/ConcreteOrderStateMachine.kt
@@ -2,17 +2,16 @@ import type.Actions
 import type.Events
 import type.States
 
-typealias Result = Map<Events, Pair<States, Actions>>
+typealias Path = Map<Events, Pair<States, Actions>>
 
 class ConcreteOrderStateMachine (
-  private val transition: Map<States, List<Result>>,
+  private val transition: Map<States, List<Path>>,
   // should be initialized
   var state: States = States.READY
 ) {
-  fun transit(event: Events): Map<States, Result> {
+  fun transit(event: Events): Map<States, Path> {
     val targetState = transition[state]?.find { it.containsKey(event) }?.get(event)?.first
-    val action =  transition[targetState]?.find { it.containsKey(event) }?.get(event)?.second
-    println("Event: $event, State: $targetState, Action: $action")
+    val action =  transition[state]?.find { it.containsKey(event) }?.get(event)?.second
 
     val result = mapOf(state to mapOf(event to Pair(targetState!!, action!!)))
     this.state = targetState
