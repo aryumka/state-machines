24edf0cae603ebffa8ecd976d201c704139693dd
aryumka <yooooar@gmail.com>
2024-03-27 17:45:24 +0900

    change message and add an action

--- src/main/kotlin/type/Actions.kt
+++ src/main/kotlin/type/Actions.kt
@@ -6,8 +6,9 @@ sealed class Actions(private val logger: Logger) {
   abstract val message: String
   fun act() { logger.info(message) }
   data class OrderPlaced(override val message: String = "order is placed", val logger: Logger): Actions(logger)
-  data class PaymentMade(override val message: String = "order is delivered", val logger: Logger): Actions(logger)
+  data class PaymentMade(override val message: String = "payment is made", val logger: Logger): Actions(logger)
   data class OrderShipped(override val message: String = "order is shipped", val logger: Logger): Actions(logger)
+  data class OrderDelivered(override val message: String = "order is delivered", val logger: Logger): Actions(logger)
   data class watingForReturn(override val message: String = "waiting for return", val logger: Logger): Actions(logger)
   data class orderCanceled(override val message: String = "order is canceled", val logger: Logger): Actions(logger)
 }
